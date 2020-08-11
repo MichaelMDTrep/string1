932b20bd0b7610f8c29c8908632bb27b9cca3b51
mtrepanier935 <MichaelMDTrep@Gmail.com>
2020-08-11 15:23:31 -0400

    new commit

--- string2.py
+++ string2.py
@@ -4,7 +4,8 @@ Kenzie assignment: String2
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "Michael Trepanier"
+__author__ = "Michael Trepanier used a source"
+"http://pythonfiddle.com/google-python-exercise-two/"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
